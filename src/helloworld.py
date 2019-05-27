a14e984a26859a5693019f2f2f54f54ddec5d7a8
Gary Zlobinskiy <garzlo22@bergen.org>
2019-05-27 18:39:39 -0400

    Fixed a couple errors

--- src/helloworld.py
+++ src/helloworld.py
@@ -11,9 +11,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('StockInfo.csv')
-X = dataset.iloc[:, 3:7].values
-y = dataset.iloc[:, 2].values
+dataset = pd.read_csv('StockInfoFinal.csv')
+X = dataset.iloc[:, 1:7].values
+y = dataset.iloc[:, 1].values
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -28,17 +28,17 @@ from keras.layers import Dense
 
 classifier = Sequential()
 
-classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu', input_dim = 4))
+classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu', input_dim = 5))
 classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = [enumerate])
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['mse'])
 
 classifier.fit(X_train, y_train, batch_size = 10, epochs = 10)
 
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred == y)
 
-new_prediction = classifier.predict(sc.transform(np.array([[180.38, 170.4499, 175.6075, 5007516.0]])))
+new_prediction = classifier.predict(sc.transform(np.array([[100,100,100,100]])))
 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
