64aba195bde9eb784aa097f97cca778941ea590e
Anthony Li <li.anthony917@gmail.com>
2019-05-29 12:29:16 -0400

    Final single-stock RNN stuffs

--- src/rnn_test/rnn_test.py
+++ src/rnn_test/rnn_test.py
@@ -34,8 +34,8 @@ y_test = np.reshape(y_test, (y_test.shape[0], 5))
 
 print("Creating model...")
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(50, return_sequences=True, input_shape=(look_back, X_train.shape[2])))
-model.add(tf.keras.layers.LSTM(20))
+model.add(tf.keras.layers.LSTM(60, return_sequences=True, input_shape=(look_back, X_train.shape[2])))
+model.add(tf.keras.layers.LSTM(25))
 model.add(tf.keras.layers.Dense(5))
 model.compile(loss="mean_squared_error", optimizer="adam", metrics=["mae"])
 
