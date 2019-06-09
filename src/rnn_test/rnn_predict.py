707b26b875af28a153a0eb1d55728143f781cbec
Gary Zlobinskiy <garzlo22@bergen.org>
2019-06-09 16:23:07 -0400

    attempted to make prediction model

--- src/rnn_test/rnn_predict.py
+++ src/rnn_test/rnn_predict.py
@@ -1,14 +1,15 @@
 #https://www.marketwatch.com/game/stockpredictionrnn
 
+look_back = 60
+num_features = 5
+'''
 import cgi
 form = cgi.FieldStorage()
-'''
+
 # Use these in the prediction model
 ticker = form.getValue('tickerBox')
 time = form.getValues('timeChoice')
 '''
-
-
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -17,6 +18,9 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 import os
+
+
+
 
 values = []
 
@@ -73,33 +77,30 @@ print("\nSplitting data...")
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8)
 X_train = np.reshape(X_train, (X_train.shape[0], look_back, X_train.shape[2]))
 X_test = np.reshape(X_test, (X_test.shape[0], look_back, X_test.shape[2]))
-y_train = np.reshape(y_train, (y_train.shape[0], 5))
-y_test = np.reshape(y_test, (y_test.shape[0], 5))
+
+
+
+
+
+
+
+min_val = [15.2, 15.62, 14.87, 15.15, 3458100.0]
+max_val = [178.94, 180.38, 175.75, 179.94, 591052200.0]
 
 print("Creating model...")
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.LSTM(60, return_sequences=True, input_shape=(look_back, X_train.shape[2])))
+model.add(tf.keras.layers.LSTM(60, return_sequences=True, input_shape=(look_back, num_features)))
 model.add(tf.keras.layers.LSTM(25))
-model.add(tf.keras.layers.Dense(5))
+model.add(tf.keras.layers.Dense(num_features))
 model.compile(loss="mean_squared_error", optimizer="adam", metrics=["mae"])
 
-print("Training...")
-# TODO: Decrease batch_size
-history = model.fit(X_train, y_train, epochs=2, batch_size=1, verbose=1, validation_data=(X_test, y_test))
+model.load_weights("rnn_model.h5")
 
-print("Saving...")
-model.save_weights("rnn_model.h5")
+result = model.predict(X_test)
 
-print("Plotting...")
-plt.figure(figsize=(16, 10))
-plt.plot(history.epoch, history.history["val_mean_absolute_error"], label="Validation")
-plt.plot(history.epoch, history.history["mean_absolute_error"], label="Train")
-plt.xlabel("Epochs")
-plt.ylabel("Mean Squared Error")
-plt.legend()
+from pandas import Series
+series = Series.from_csv('rnn_test_data.csv', header = 0)
+print(series.head())
+series.add(result)
+series.plot()
 plt.show()
-
-print("Evaluating...")
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print("Test accuracy: ", test_acc)
-
