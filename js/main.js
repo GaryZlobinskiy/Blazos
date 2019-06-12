11c386d6ce8172dca5f5dee925bc597dc768a83f
Anthony Li <li.anthony917@gmail.com>
2019-06-12 14:57:22 -0400

    Significant upgrades

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ function ok(message) {
 const minVal = [15.2, 15.62, 14.87, 15.15, 3458100.0];
 const maxVal = [178.94, 180.38, 175.75, 179.94, 591052200.0];
 
+let alphaVantageApiKey;
+
 info("Loading model...");
 const modelPromise = tf.loadLayersModel("src/rnn_test/rnn_model_js/model.json").then(model => {
     window.model = model;
@@ -26,143 +28,205 @@ async function getModel() {
 
 function unscale(row) {
     return row.map((val, idx) => {
-       return val * (maxVal[idx] - minVal[idx]) + minVal[idx];
+        return val * (maxVal[idx] - minVal[idx]) + minVal[idx];
     });
 }
 
-async function predict(data, days, cb) {
+function predict(data, cb, timeout = 1) {
     info("Trimming and scaling data...");
-    let trimmedData = data.slice(-240).map(row => row.slice(1).map((val, idx) => {
+    let trimmedData = data.slice(-240).map(row => row.map((val, idx) => {
         return (val - minVal[idx]) / (maxVal[idx] - minVal[idx]);
     }));
 
     info("Running model...");
-    let i = 0;
-    function run(done) {
-        console.log(`Running model (${i + 1}/${days})...`);
+    function run() {
+        console.log(`Running model...`);
         const input = tf.tensor([trimmedData.slice(-240).flat()]);
         const output = model.predict(input).dataSync();
 
         trimmedData.push(Array.from(output));
-        cb(data.length + i, unscale(output));
-        i++;
-
-        if (i < days) {
-            setTimeout(() => run(done), 1);
-        } else {
-            done();
+        if (!cb(unscale(output))) {
+            setTimeout(run, timeout);
         }
     }
 
-    await new Promise(res => {
-       run(res);
-    });
-
-    ok("Done predicting.");
-    return trimmedData.slice(240).map(unscale);
+    run();
 }
 
 Vue.component("sdf-stock", {
     props: ["symbol"],
     data() {
         return {
-            isLoaded: false,
             error: false,
             chart: null,
             data: null,
-            featureIndex: 1
+            predicted: [],
+            featureIndex: 1,
+            isPredicting: false
         };
     },
     mounted() {
-        info(`Loading data for ${this.$props.symbol}...`);
-
-        fetch(`src/rnn_test/data/${this.$props.symbol}.csv`).then(async response => {
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-
-            this.isLoaded = true;
-
-            ok(`Loaded data for ${this.$props.symbol}.`);
-            info(`Parsing data for ${this.$props.symbol}...`);
-
-            const text = await response.text();
-            this.data = text.split("\n").slice(1).map(line => {
-                const cols = line.split(",");
-
-                const date = new Date(cols[0] + " GMT");
-
-                return [date.getTime() / (24 * 60 * 60 * 1000), ...cols.slice(1).map(parseFloat)];
-            }).filter(row => row.length === 6);
-
-            info(`Creating chart for ${this.$props.symbol}...`);
-
-            const graphLookBack = 3128;
-            const chartData = {
-                datasets: [{
-                    data: this.data.slice(-graphLookBack).map((line, index) => {return {x: index + this.data.length - graphLookBack, y: line[this.featureIndex + 1]}}),
-                    xAxisId: "x-axis",
-                    label: "Actual",
-                    borderColor: "rgb(24, 128, 255)",
-                    backgroundColor: "rgba(24, 128, 255, 0.2)",
-                    borderWidth: 2
-                }, {
-                    data: [],
-                    xAxisId: "x-axis",
-                    label: "Predicted",
-                    borderColor: "rgb(24, 170, 128)",
-                    backgroundColor: "rgba(24, 170, 128, 0.2)",
-                    borderWidth: 2
-                }]
-            };
-
-            this.chart = new Chart(this.$refs.chartCanvas.getContext("2d"), {
-                type: "line",
-                data: chartData,
-                options: {
-                    scales: {
-                        xAxes: [{
-                            id: "x-axis",
-                            type: "linear",
-                            position: "bottom"
-                        }]
-                    },
-                    elements: {
-                        point: {radius: 0}
-                    }
-                }
-            });
-
-            ok(`Parsed data and created chart for ${this.$props.symbol}.`);
-
-            const days = 365;
-            info(`Predicting over ${days} days - ${this.$props.symbol}...`);
-            await predict(this.data, days, (index, data) => {
-                this.chart.data.datasets[1].data.push({x: index, y: data[this.featureIndex]});
-                this.chart.update();
-            });
-        }).catch(e => {
-            console.log(e);
-            this.error = true;
-        });
+        this.reload();
     },
-    computed: {
-        canvasStyle() {
-            return this.isLoaded ? {display: "block"} : {display: "none"};
+    methods: {
+        async reload() {
+            this.error = false;
+            try {
+                info(`Loading data for ${this.$props.symbol}...`);
+
+                let response = await fetch(`src/rnn_test/data/${this.$props.symbol}.csv`);
+                if (response.status === 404) {
+                    response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${this.$props.symbol}&apikey=${alphaVantageApiKey}&outputsize=full&datatype=csv`)
+                }
+
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+
+                const text = await response.text();
+
+                ok(`Loaded data for ${this.$props.symbol}.`);
+                info(`Parsing data for ${this.$props.symbol}...`);
+
+                this.data = text.split("\n").slice(1).map(line => {
+                    const cols = line.split(",");
+                    return cols.slice(1).map(parseFloat);
+                }).filter(row => row.length === 5);
+
+                info(`Creating chart for ${this.$props.symbol}...`);
+
+                const graphLookBack = 720;
+                const chartData = {
+                    datasets: [{
+                        data: this.data.slice(-graphLookBack).map((line, index) => {
+                            return {
+                                x: index + this.data.length - graphLookBack,
+                                y: line[this.featureIndex + 1]
+                            }
+                        }),
+                        xAxisId: "x-axis",
+                        label: "Actual",
+                        borderColor: "rgb(24, 128, 255)",
+                        backgroundColor: "rgba(24, 128, 255, 0.2)",
+                        borderWidth: 2
+                    }, {
+                        data: [],
+                        xAxisId: "x-axis",
+                        label: "Predicted",
+                        borderColor: "rgb(24, 170, 128)",
+                        backgroundColor: "rgba(24, 170, 128, 0.2)",
+                        borderWidth: 2
+                    }]
+                };
+
+                this.chart = new Chart(this.$refs.chartCanvas.getContext("2d"), {
+                    type: "line",
+                    data: chartData,
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                id: "x-axis",
+                                type: "linear",
+                                position: "bottom",
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: "Data Index"
+                                }
+                            }],
+                            yAxes: [{
+                                id: "y-axis",
+                                type: "linear",
+                                position: "left",
+                                gridLines: {
+                                    color: "rgba(255, 255, 255, 0.1)",
+                                    zeroColor: "rgba(255, 255, 255, 0.2)"
+                                },
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: "Share Price"
+                                },
+                                ticks: {
+                                    callback: (value, index, values) => {
+                                        return "$" + value;
+                                    }
+                                }
+                            }]
+                        },
+                        tooltips: {
+                            callbacks: {
+                                title() {},
+                                label: (item, data) => {
+                                    let label = "$" + Math.round(item.yLabel * 100) / 100;
+                                    if (label.split(".")[1].length < 2) {
+                                        label += "0";
+                                    }
+                                    return label;
+                                }
+                            }
+                        },
+                        elements: {
+                            point: {radius: 0}
+                        }
+                    }
+                });
+
+                ok(`Parsed data and created chart for ${this.$props.symbol}.`);
+            } catch (e) {
+                console.error(e);
+                this.error = e;
+            }
+        },
+        startPredicting(days, offset = 0) {
+            if (!this.isPredicting) {
+                this.isPredicting = true;
+                info(`Predicting over ${days} days - ${this.$props.symbol}...`);
+                let i = 0;
+                predict(this.data.slice(0, -(offset + 1)).concat(this.predicted), data => {
+                    this.predicted.push(Array.from(data));
+                    this.chart.data.datasets[1].data.push({x: this.data.length - offset + this.predicted.length - 1, y: data[this.featureIndex]});
+                    this.chart.update({duration: 0});
+
+                    i++;
+                    if (i >= days) {
+                        this.isPredicting = false;
+                    }
+                    return !this.isPredicting;
+                });
+            }
+        },
+        clear() {
+            this.$emit("remove");
         }
     },
     // language=HTML
     template: `
-        <div>
-          <hr />
-          <div class="row" >
-            <div class="col-md-2"><h3>{{symbol}}</h3></div>
-            <div class="col">
-              <canvas ref="chartCanvas" class="w-100 chart" v-bind:style="canvasStyle"></canvas>
-              <span class="text-danger" v-if="error">Failed to load stock data</span>
+      <div>
+        <hr />
+        <div class="row" >
+          <div class="col-md-2">
+            <h3>{{symbol}}</h3>
+            <div v-if="data">
+              <p v-if="isPredicting"><a href="#" @click.prevent="isPredicting = false">Stop Predicting</a></p>
+              <div v-else>
+                <p><a href="#" @click.prevent="startPredicting(30)">Predict 1 Month</a></p>
+                <p><a href="#" @click.prevent="startPredicting(90)">Predict 3 Months</a></p>
+                <p><a href="#" @click.prevent="startPredicting(180)">Predict 6 Months</a></p>
+                <p><a href="#" @click.prevent="startPredicting(365)">Predict 1 Year</a></p>
+              </div>
+            </div>
+            <p v-else class="text-secondary">Loading data...</p>
+            <p><a href="#" @click.prevent="clear()">Clear</a></p>
+          </div>
+          <div class="col">
+            <canvas ref="chartCanvas" class="w-100 chart"></canvas>
+            <div v-if="error">
+              <p class="text-danger">Failed to load stock data. Try setting the Alpha Vantage API key or using a different symbol.</p>
+              <p class="text-danger">Full error message: {{error.message}}</p>
+              <p><a href="#" @click.prevent="reload()">Try again?</a></p>
             </div>
           </div>
         </div>
+      </div>
     `,
 
 });
@@ -171,7 +235,7 @@ const app = new Vue({
     el: "#app",
     data: function () {
         return {
-            ticker: "AAPL",
+            ticker: "AMZN",
             tickers: []
         }
     },
@@ -182,9 +246,12 @@ const app = new Vue({
         addTicker(event) {
             event.preventDefault();
             if (!this.tickers.includes(this.ticker.toUpperCase())) {
-                event.target.reset();
-                this.tickers.push(this.ticker.toUpperCase());
+                this.$refs.addTickerForm.reset();
+                this.tickers.splice(0, 0, this.ticker.toUpperCase());
             }
+        },
+        getAPIKey() {
+            alphaVantageApiKey = prompt("Enter the Alpha Vantage API key.") || alphaVantageApiKey;
         }
     }
 });
